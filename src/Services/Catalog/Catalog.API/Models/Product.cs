4e2f2cd4299b8ac2731aff91d7fa657c76e2dbd9
patrickr3 <patrickr3@gmail.com>
2025-02-24 08:09:06 -0400

    Adding Catalog Microservices

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Bussiness logic to create product
+        // Create Product entity from command object
+        Product product = new()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO: Save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        // Return CreateProductResult result
+        return new CreateProductResult(product.Id);
+    }
+}
